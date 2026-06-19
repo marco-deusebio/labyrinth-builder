5b826c8232c82ddec90ec8977cb4ac1a38df7f1a
Marco D'Eusebio <marco_deusebio@yahoo.com>
2026-06-18 22:55:34 -0700

    Initial Commit

---
+++ Labyrinth Forge/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Labyrinth Forge
+//
+//  Created by Marco D’Eusebio on 6/18/26.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return EXIT_SUCCESS;
+}
